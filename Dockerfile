307bb837832a7be2c92fe68a9718916d9f59d282
Matan Cohen <matan.c@walkme.com>
2020-03-30 15:59:00 +0300

    Adding skaffold

--- Dockerfile
+++ Dockerfile
@@ -15,5 +15,4 @@ RUN npm install
 # Bundle app source
 COPY ./ ./
 
-
 CMD [ "npm", "start" ]
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  console.log(new Date());
-  res.send("Hello World!7");
+  
+  res.send("Hello World! " + new Date());
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
