6f8bfd13ac2604ebaad34f4b5e0858703b1276e9
Matan Cohen <matan.c@walkme.com>
2020-03-30 16:46:20 +0300

    Naming

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  
-  res.send("Hello World! " + new Date());
+  res.send("Hello World! 2" + new Date());
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
