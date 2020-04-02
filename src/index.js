b0ac137cba94476d02a08cf3a97f5da50c914ed5
Matan Cohen <matandcohen@gmail.com>
2020-04-02 12:02:48 +0300

    Fixing dashboard

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const uuid = require("uuid");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!3" + uuid.v1());
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
