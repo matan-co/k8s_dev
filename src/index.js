9ff9527e0248ea6ae5f838d261bb9e8e6467c96f
Matan Cohen <matandcohen@gmail.com>
2020-04-12 18:04:41 +0300

    Change skaffold deployment working with helm + Adding debug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const uuid = require("uuid");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!3" + uuid.v1());
+  res.send("Hello World!");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
