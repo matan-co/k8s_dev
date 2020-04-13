4f62c15d96fa998842e9305dda08749b73eca464
Matan Cohen <matandcohen@gmail.com>
2020-04-13 11:26:55 +0300

    Reading values from configmap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.NODE_PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World2!");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
