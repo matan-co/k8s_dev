ad600ffc0d4b84e07ace8eac0213eeaf5e918eae
Matan Cohen <matandcohen@gmail.com>
2020-04-13 15:38:03 +0300

    Reading secretes from file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.NODE_PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World2!");
+  res.send("SECRET : " + process.env.SECRET_USERNAME);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
