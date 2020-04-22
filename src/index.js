2a7f88275f60a73b0a630fb5602440a915fbb0a2
Matan Cohen <matandcohen@gmail.com>
2020-04-22 14:56:37 +0300

    Reading secrets from range without encryption

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = process.env.NODE_PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("SECRET : " + process.env.SECRET_USERNAME);
+  res.send(`${process.env.USERNAME} \n
+            ${process.env.PASSWORD}`);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
